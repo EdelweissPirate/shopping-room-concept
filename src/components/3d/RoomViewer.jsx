178e2e25797b07903e7d7d50452e6ac2fad3afc3
EdelweissPirate <joshmarkevans@gmail.com>
2023-01-13 21:11:53 -0600

    deeeeplooooy

--- src/components/3d/RoomViewer.jsx
+++ src/components/3d/RoomViewer.jsx
@@ -27,8 +27,8 @@ function RoomViewer({ onClick }) {
         zoom: 1.6 //DESKTOP: 2
     }
 
-    // const texture = useLoader(RGBELoader, 'src/assets/the_sky_is_on_fire_1k.hdr')
-    // texture.mapping = THREE.EquirectangularReflectionMapping
+    const texture = useLoader(RGBELoader, './the_sky_is_on_fire_1k.hdr') //'src/assets/the_sky_is_on_fire_1k.hdr'
+    texture.mapping = THREE.EquirectangularReflectionMapping
     
     return (
         <section>
@@ -36,7 +36,7 @@ function RoomViewer({ onClick }) {
                 <CameraController />
                 <ambientLight intensity={0.4} color={'#FF9470'} />
                 <spotLight color={'#FF9470'} position={[-8, 6, 0]} angle={0.3} penumbra={.2} intensity={1}/>
-                {/* <Environment  near={1} far={1000} resolution={256} map={texture} /> */}
+                <Environment  near={1} far={1000} resolution={256} map={texture} />
                 <group position={[0, 0, 0]}>
                     <Center>
                         <Suspense fallback={'Loading...'}>
